a5b8b6ce70ed4e5fef16672a87d8a0aa18a07bd4
Julius Walton <juliuswalton7901@gmail.com>
2025-07-27 01:17:59 -0400

    Add support for go

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,102 +1,109 @@
 return {
-  'neovim/nvim-lspconfig',
-  dependencies = {
-    { 'williamboman/mason.nvim', opts = {} },
-    'williamboman/mason-lspconfig.nvim',
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
-    { 'j-hui/fidget.nvim', opts = {} },
-    'hrsh7th/cmp-nvim-lsp',
-  },
-  config = function()
-    if vim.g.have_nerd_font then
-      local signs = { ERROR = ' ', WARN = ' ', INFO = ' ', HINT = '󰌵 ' }
-      local diagnostic_signs = {}
-      for type, icon in pairs(signs) do
-        diagnostic_signs[vim.diagnostic.severity[type]] = icon
-      end
-      vim.diagnostic.config { signs = { text = diagnostic_signs } }
-    end
+	'neovim/nvim-lspconfig',
+	dependencies = {
+		{ 'williamboman/mason.nvim', opts = {} },
+		'williamboman/mason-lspconfig.nvim',
+		'WhoIsSethDaniel/mason-tool-installer.nvim',
+		{ 'j-hui/fidget.nvim',       opts = {} },
+		'hrsh7th/cmp-nvim-lsp',
+	},
+	config = function()
+		if vim.g.have_nerd_font then
+			local signs = { ERROR = ' ', WARN = ' ', INFO = ' ', HINT = '󰌵 ' }
+			local diagnostic_signs = {}
+			for type, icon in pairs(signs) do
+				diagnostic_signs[vim.diagnostic.severity[type]] = icon
+			end
+			vim.diagnostic.config { signs = { text = diagnostic_signs } }
+		end
 
-    -- LSP capabilities
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+		-- LSP capabilities
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
-    -- LSP Attach autocmd for keymaps
-    vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
-      callback = function(event)
-        local map = function(keys, func, desc, mode)
-          mode = mode or 'n'
-          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-        end
+		-- LSP Attach autocmd for keymaps
+		vim.api.nvim_create_autocmd('LspAttach', {
+			group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+			callback = function(event)
+				local map = function(keys, func, desc, mode)
+					mode = mode or 'n'
+					vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+				end
 
-        local has_telescope, builtin = pcall(require, 'telescope.builtin')
-        
-        if has_telescope then
-          map('gd', builtin.lsp_definitions, '[G]oto [D]efinition')
-          map('gr', builtin.lsp_references, '[G]oto [R]eferences')
-          map('gI', builtin.lsp_implementations, '[G]oto [I]mplementation')
-          map('<leader>D', builtin.lsp_type_definitions, 'Type [D]efinition')
-          map('<leader>ds', builtin.lsp_document_symbols, '[D]ocument [S]ymbols')
-          map('<leader>ws', builtin.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-        else
-          map('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-          map('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
-          map('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-        end
+				local has_telescope, builtin = pcall(require, 'telescope.builtin')
 
-        map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
-        map('K', vim.lsp.buf.hover, 'Hover Documentation')
-        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-      end,
-    })
+				if has_telescope then
+					map('gd', builtin.lsp_definitions, '[G]oto [D]efinition')
+					map('gr', builtin.lsp_references, '[G]oto [R]eferences')
+					map('gI', builtin.lsp_implementations, '[G]oto [I]mplementation')
+					map('<leader>D', builtin.lsp_type_definitions, 'Type [D]efinition')
+					map('<leader>ds', builtin.lsp_document_symbols, '[D]ocument [S]ymbols')
+					map('<leader>ws', builtin.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+				else
+					map('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+					map('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
+					map('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+				end
 
-    local ensure_installed = {
-      'lua_ls',
-      'omnisharp',
-      'vtsls',     -- For Vue 3
-      'vetur-vls', -- For Vue 2 (Corrected Name)
-      'stylua',
-      'prettier',
-    }
-    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+				map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+				map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+				map('K', vim.lsp.buf.hover, 'Hover Documentation')
+				map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+				map('<leader>f', vim.lsp.buf.format, '[F]ormat Document')
+			end,
+		})
 
-    local lspconfig = require 'lspconfig'
+		local ensure_installed = {
+			'lua_ls',
+			'omnisharp',
+			'vtsls', -- For Vue 3
+			'vetur-vls', -- For Vue 2 (Corrected Name)
+			'stylua',
+			'prettier',
+			'gopls',
+		}
+		require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
-    require('mason-lspconfig').setup {
-      handlers = {
-        ['vue_ls'] = function() end,
+		local lspconfig = require 'lspconfig'
 
-        ['vtsls'] = function()
-          lspconfig.vtsls.setup {
-            capabilities = capabilities,
-            filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
-          }
-        end,
+		require('mason-lspconfig').setup {
+			handlers = {
+				['vue_ls'] = function() end,
 
-        ['vetur'] = function()
-          lspconfig.vetur.setup {
-            capabilities = capabilities,
-          }
-        end,
+				['vtsls'] = function()
+					lspconfig.vtsls.setup {
+						capabilities = capabilities,
+						filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+					}
+				end,
 
-        ['lua_ls'] = function()
-          lspconfig.lua_ls.setup {
-            capabilities = capabilities,
-            settings = {
-              Lua = {
-                diagnostics = { globals = { 'vim' } },
-              },
-            },
-          }
-        end,
-        ['omnisharp'] = function()
-          lspconfig.omnisharp.setup {
-            capabilities = capabilities,
-          }
-        end,
-      },
-    }
-  end,
+				['vetur'] = function()
+					lspconfig.vetur.setup {
+						capabilities = capabilities,
+					}
+				end,
+
+				['lua_ls'] = function()
+					lspconfig.lua_ls.setup {
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								diagnostics = { globals = { 'vim' } },
+							},
+						},
+					}
+				end,
+				['omnisharp'] = function()
+					lspconfig.omnisharp.setup {
+						capabilities = capabilities,
+					}
+				end,
+				['gopls'] = function()
+					lspconfig.gopls.setup {
+						capabilities = capabilities
+					}
+				end,
+			},
+		}
+	end,
 }
