500636d86b9e49295642a7e91afeccc008f37999
Julius Walton <julius@cognitoforms.com>
2026-02-27 19:05:51 -0500

    Sync Neovim config updates and defaults

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,6 +5,8 @@
 vim.g.autoformat = false
 vim.opt.fixendofline = false
 vim.opt.endofline = false
+vim.opt.fileformat = "dos"
+vim.opt.fileformats = { "dos", "unix" }
 
 local dracula_pro_path = vim.fn.stdpath("data") .. "/site/pack/themes/start/dracula_pro"
 if (vim.uv or vim.loop).fs_stat(dracula_pro_path) then
